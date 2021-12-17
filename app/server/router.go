1c00109dd45ba9b74c08d32b5ca01099e24c617c
kimchi <pavel@nate.tech>
2021-12-17 16:57:54 +0000

    chore: docker build auth

--- app/server/router.go
+++ app/server/router.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"net/http"
+
 	"blitzshare.api/app/dependencies"
 	"blitzshare.api/app/server/endpoints"
 	"github.com/gin-gonic/gin"
@@ -12,7 +14,12 @@ func NewRouter(deps *dependencies.Dependencies) *gin.Engine {
 		gin.SetMode(gin.ReleaseMode)
 	}
 
+	router.NoRoute(func(c *gin.Context) {
+		endpoints.AddDefaultResponseHeaders(c)
+		c.JSON(http.StatusNotFound, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found :("})
+	})
 	router.GET("/test", endpoints.HealthHandler())
+	// TODO: cleanup
 	//router.GET("/file-share-link", endpoints.FileShareHandler(deps))
 	router.POST("/p2p/registry", endpoints.RegisterP2pPeerHandler(deps))
 	router.GET("/p2p/registry/:id", endpoints.GetP2pPeerHandler(deps))
