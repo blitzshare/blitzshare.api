22ad46221f99200a4bd908c80ead9d95a2241f01
Pavel Durov <pav3ldurov@gmail.com>
2022-02-18 21:08:22 +0000

    fix: ci tests

--- app/routes/getBootstapNode_handler_test.go
+++ app/routes/getBootstapNode_handler_test.go
@@ -3,6 +3,7 @@ package routes_test
 import (
 	"blitzshare.api/app/model"
 	"encoding/json"
+	"github.com/stretchr/testify/mock"
 	"io/ioutil"
 	"net/http/httptest"
 
@@ -24,15 +25,42 @@ var _ = Describe("Get /p2p/bootstrap-node", func() {
 			Server:   config.Server{Port: 323},
 			Settings: config.Settings{RedisUrl: "redis.svc.cluster.local"},
 		}
+		It("expected 401 Unauthorized", func() {
+			rec := httptest.NewRecorder()
+			c, _ := gin.CreateTestContext(rec)
+			registry := &mocks.Registry{}
+			registry.On("GetNodeConfig").Return("", nil)
+			keychain := &mocks.ApiKeychain{}
+			keychain.On("IsValid", mock.MatchedBy(test.MatchAny)).Return(false)
+			deps := dependencies.Dependencies{
+				Config:      config,
+				Registry:    registry,
+				ApiKeychain: keychain,
+			}
+			c.Request = &http.Request{
+				Header: make(http.Header),
+			}
+			c.Request.Header.Set("x-api-key", "test")
+			routes.GetBootstrapNodeHandler(&deps)(c)
+			Expect(rec.Code).To(Equal(http.StatusUnauthorized))
+			test.AsserBlitzshareHeaders(rec)
+		})
 		It("expected 400 NotFound", func() {
 			rec := httptest.NewRecorder()
 			c, _ := gin.CreateTestContext(rec)
 			registry := &mocks.Registry{}
 			registry.On("GetNodeConfig").Return("", nil)
+			keychain := &mocks.ApiKeychain{}
+			keychain.On("IsValid", mock.MatchedBy(test.MatchAny)).Return(true)
 			deps := dependencies.Dependencies{
-				Config:   config,
-				Registry: registry,
+				Config:      config,
+				Registry:    registry,
+				ApiKeychain: keychain,
 			}
+			c.Request = &http.Request{
+				Header: make(http.Header),
+			}
+			c.Request.Header.Set("x-api-key", "test")
 			routes.GetBootstrapNodeHandler(&deps)(c)
 			Expect(rec.Code).To(Equal(http.StatusNotFound))
 			test.AsserBlitzshareHeaders(rec)
@@ -41,25 +69,38 @@ var _ = Describe("Get /p2p/bootstrap-node", func() {
 			rec := httptest.NewRecorder()
 			c, _ := gin.CreateTestContext(rec)
 			registry := &mocks.Registry{}
+			keychain := &mocks.ApiKeychain{}
+			keychain.On("IsValid", mock.MatchedBy(test.MatchAny)).Return(true)
 			registry.On("GetNodeConfig").Return("not-parsable-id", nil)
 			deps := dependencies.Dependencies{
-				Config:   config,
-				Registry: registry,
+				Config:      config,
+				Registry:    registry,
+				ApiKeychain: keychain,
 			}
+			c.Request = &http.Request{
+				Header: make(http.Header),
+			}
+			c.Request.Header.Set("x-api-key", "test")
 			routes.GetBootstrapNodeHandler(&deps)(c)
 			Expect(rec.Code).To(Equal(http.StatusInternalServerError))
 			test.AsserBlitzshareHeaders(rec)
 		})
-
 		It("expected 200 Ok for valid OTP", func() {
 			registry := &mocks.Registry{}
 			registry.On("GetNodeConfig").Return("{\"nodeId\":\"note-test-id\",\"port\":63785}", nil)
+			keychain := &mocks.ApiKeychain{}
+			keychain.On("IsValid", mock.MatchedBy(test.MatchAny)).Return(true)
 			deps := dependencies.Dependencies{
-				Config:   config,
-				Registry: registry,
+				Config:      config,
+				Registry:    registry,
+				ApiKeychain: keychain,
 			}
 			rec := httptest.NewRecorder()
 			c, _ := gin.CreateTestContext(rec)
+			c.Request = &http.Request{
+				Header: make(http.Header),
+			}
+			c.Request.Header.Set("x-api-key", "test")
 			routes.GetBootstrapNodeHandler(&deps)(c)
 
 			body, _ := ioutil.ReadAll(rec.Body)
